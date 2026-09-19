491618e9e3861d4c97d0e80db5886dfc57653746
grassman047 <nik.kiriyak.17@mail.ru>
2026-09-19 11:13:57 +0300

    Refactor bot.py for improved structure and clarity

--- bot.py
+++ bot.py
@@ -81,11 +81,15 @@ async def create_button(interaction: discord.Interaction):
         await interaction.response.send_message(f"❌ Команда только в <#{FIRST_CHANNEL_ID}>", ephemeral=True)
         return
     
+    # СНАЧАЛА ОТВЕЧАЕМ НА КОМАНДУ (чтобы Discord не выдал "Приложение не отвечает")
+    await interaction.response.send_message("✅ Кнопка создана!", ephemeral=True)
+    
+    # ПОТОМ ОТПРАВЛЯЕМ СООБЩЕНИЕ С КНОПКОЙ
     embed = discord.Embed(
         title="🔰 Запросить помощь против тиммеров в JJS 🔰",
         description=(
             "❓ Чтобы запросить помощь, нажмите кнопку ниже и укажите свой юзернейм в Roblox ❓\n\n"
-            "⚡ Важно, чтобы вы сделали в настройках роблокса всё как на скришноте ниже "
+            "⚡ Важно, чтобы вы сделали в настройках роблокса всё как на скриншоте ниже "
             "(это надо для присоединения на ваш сервер) ⚡\n\n"
             "⚠️ **Запрещено** ⚠️ :\n"
             "•  Спамить запросами\n"
@@ -97,7 +101,6 @@ async def create_button(interaction: discord.Interaction):
     )
     embed.set_image(url="https://media.discordapp.net/attachments/1039182671710007296/1549863612233941174/image_3.png?ex=6aac3e78&is=6aaaecf8&hm=683d3c8051c37f9806646148f5cf78cc5dcc67218e2bf661d4b70035cbfc2ace&=&format=webp&quality=lossless")
     await interaction.channel.send(embed=embed, view=NickButtonView())
-    await interaction.response.send_message("✅ Кнопка создана!", ephemeral=True)
 
 @bot.event
 async def on_ready():
